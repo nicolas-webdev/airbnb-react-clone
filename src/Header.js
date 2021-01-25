e2bb0371407ab0827c7178d0bc51088439c04546
Nicolas <nicobsas+webdev@gmail.com>
2021-01-26 02:04:07 +0900

    mobile styling

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import LanguageIcon from "@material-ui/icons/Language";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import { Avatar } from "@material-ui/core";
 
 function Header() {
@@ -24,7 +23,6 @@ function Header() {
       <div className="header__right">
         <p>Become a host</p>
         <LanguageIcon />
-        <ExpandMoreIcon />
         <Avatar />
       </div>
     </div>
